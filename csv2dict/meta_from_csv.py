c63c16b647ad1da74c789dcc5f13c04ca5a44108
Dawn Childress <kirschbombe@gmail.com>
2017-08-31 15:02:58 -0700

    Add support for repeatable fields

--- csv2dict/meta_from_csv.py
+++ csv2dict/meta_from_csv.py
@@ -30,19 +30,39 @@ def process_rows( csv2dict):
         n = csv2dict.new_dict(row['File path'])
 
         csv2dict.set_title(row['Title'], n)
-        csv2dict.set_alt_title(row['Alternative Title'], n)
+
+        num = 1
+        while 'Alternative Title %d' % num in row.keys():
+            csv2dict.set_collection(row['Alternative Title %d' % num], n)
+            num += 1
+
         csv2dict.set_id(row['Identifier'], n)
-        csv2dict.set_local_id(row['Local Identifier'], n)
+
+        num = 1
+        while 'Local Identifier %d' % num in row.keys():
+            csv2dict.set_collection(row['Local Identifier %d' % num], n)
+            num += 1
+
         csv2dict.set_type(row['Type'], n)
-        csv2dict.set_campus_unit(row['Campus/Unit'], n)
 
-        csv2dict.set_date(row['Date'],
-                          row['Date Type'],
-                          row['Inclusive Start'],
-                          row['Inclusive End'],
-                          row['Single'], n)
+        num = 1
+        while 'Campus/Unit %d' % num in row.keys():
+            csv2dict.set_collection(row['Campus/Unit %d' % num], n)
+            num += 1
 
-        csv2dict.set_publication_origination(row['Publication/Origination Info'], n)
+        num = 1
+        while 'Date %d' % num in row.keys():
+            csv2dict.set_creator(row['Date %d' % num],
+                                 row['Date %d Type' % num],
+                                 row['Inclusive Start'],
+                                 row['Inclusive End'],
+                                 row['Single'], n)
+            num += 1
+
+        num = 1
+        while 'Publication/Origination Info %d' % num in row.keys():
+            csv2dict.set_collection(row['Publication/Origination Info %d' % num], n)
+            num += 1
 
         num = 1
         while 'Creator %d Name' % num in row.keys():
@@ -53,11 +73,14 @@ def process_rows( csv2dict):
                                  row['Creator %d Authority ID' % num], n)
             num += 1
 
-        csv2dict.set_contributor(row['Contributor Name'],
-                                 row['Contributor Name Type'],
-                                 row['Contributor Role'],
-                                 row['Contributor Source'],
-                                 row['Contributor Authority ID'], n)
+        num = 1
+        while 'Contributor %d Name' % num in row.keys():
+            csv2dict.set_contributor(row['Contributor %d Name' % num],
+                                 row['Contributor %d Name Type' % num],
+                                 row['Contributor %d Role' % num],
+                                 row['Contributor %d Source' % num],
+                                 row['Contributor %d Authority ID' % num], n)
+            num += 1
 
         csv2dict.set_physdesc(row['Format/Physical Description'], n)
 
@@ -68,17 +91,31 @@ def process_rows( csv2dict):
             num += 1
 
         csv2dict.set_extent(row['Extent'], n)
-        csv2dict.set_language(row['Language'],
-                              row['Language Code'], n)
-        csv2dict.set_temporal_coverage(row['Temporal Coverage'], n)
+
+        num = 1
+        while 'Language %d' % num in row.keys():
+            csv2dict.set_language(row['Language %d' % num],
+                                     row['Language %d Code' % num], n)
+            num += 1
+
+        num = 1
+        while 'Temporal Coverage %d' % num in row.keys():
+            csv2dict.set_collection(row['Temporal Coverage %d' % num], n)
+            num += 1
+
         csv2dict.set_transcription(row['Transcription'], n)
         csv2dict.set_access_restrictions(row['Access Restrictions'], n)
         csv2dict.set_rights_statement(row['Copyright Statement'], n)
         csv2dict.set_rights_status(row['Copyright Status'], n)
-        csv2dict.set_rights_holder(row['Copyright Holder Name'],
-                                   row['Copyright Holder Name Type'],
-                                   row['Copyright Holder Source'],
-                                   row['Copyright Holder Authority ID'], n)
+
+        num = 1
+        while 'Copyright Holder %d Name' % num in row.keys():
+            csv2dict.set_rights_holder(row['Copyright Holder %d Name' % num],
+                                   row['Copyright Holder %d Name Type' % num],
+                                   row['Copyright Holder %d Source' % num],
+                                   row['Copyright Holder %d Authority ID' % num], n)
+            num += 1
+
         csv2dict.set_rights_contact(row['Copyright Contact'], n)
         csv2dict.set_rights_notice(row['Copyright Notice'], n)
         csv2dict.set_rights_determination_date(row['Copyright Determination Date'], n)
@@ -92,7 +129,11 @@ def process_rows( csv2dict):
             csv2dict.set_collection(row['Collection %d' % num], n)
             num += 1
 
-        csv2dict.set_related_resource(row['Related Resource'], n)
+        num = 1
+        while 'Related Resource %d' % num in row.keys():
+            csv2dict.set_collection(row['Related Resource %d' % num], n)
+            num += 1
+
         csv2dict.set_source(row['Source'], n)
 
         num = 1
@@ -128,7 +169,11 @@ def process_rows( csv2dict):
                                     row['Form/Genre %d Authority ID' % num], n)
             num += 1
 
-        csv2dict.set_provenance(row['Provenance'], n)
+        num = 1
+        while 'Provenance %d' % num in row.keys():
+            csv2dict.set_collection(row['Provenance %d' % num], n)
+            num += 1
+
         csv2dict.set_physical_location(row['Physical Location'], n)
 
 def main(argv):
